bd07cd895744d7d4b73475e5f4d775198f712552
SourabhK0625 <106949506+SourabhK0625@users.noreply.github.com>
2022-07-28 13:30:34 +0530

    Increment by 5

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,13 @@ const counterReducer = (state ={counter : 0} , action)=>{
     if(action.type === 'increment')
     {
         return{
-            counter: state.counter+2
+            counter: state.counter+5
         }
     }
     if(action.type === 'decrement')
     {
         return{
-            counter: state.counter-2
+            counter: state.counter-5
         }
     }
     return state;
